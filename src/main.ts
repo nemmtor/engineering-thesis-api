18bd0c3c34fb6605ed6245b63d7843f9b66e1f16
nemmtor <kacper0witas@gmail.com>
2021-09-12 02:24:43 +0200

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
+  Sentry.init({
+    dsn: 'https://99c2626090d741d99fd245e85023d9d0@o996703.ingest.sentry.io/5955194',
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('SalesHelper API')
