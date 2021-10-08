f9c76fb4c27088b9bf08f8eef2bda006b70a77aa
nemmtor <kacper0witas@gmail.com>
2021-10-08 14:39:01 +0200

    send only 500 errors to sentry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { HttpException, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { RavenInterceptor } from 'nest-raven';
@@ -18,7 +18,16 @@ import { UserModule } from './user/user.module';
   providers: [
     {
       provide: APP_INTERCEPTOR,
-      useValue: new RavenInterceptor({ request: true, transaction: true }),
+      useValue: new RavenInterceptor({
+        request: true,
+        transaction: true,
+        filters: [
+          {
+            type: HttpException,
+            filter: (exception: HttpException) => exception.getStatus() < 500,
+          },
+        ],
+      }),
     },
   ],
 })
