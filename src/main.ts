68de32e7f1d66b495b9a1f38528763e45fcf45f0
nemmtor <kacper0witas@gmail.com>
2021-10-11 14:23:44 +0200

    change swagger path

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   app.use(helmet());
 
