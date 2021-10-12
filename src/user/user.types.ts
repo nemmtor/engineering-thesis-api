1f23b6e5535294cf53e676b6dd1f23e58be30c89
nemmtor <kacper0witas@gmail.com>
2021-10-12 13:55:13 +0200

    add get all users params

---
+++ src/user/user.types.ts
@@ -0,0 +1,9 @@
+import { User } from '@prisma/client';
+
+export type UserSelect = Record<keyof Omit<User, 'password'>, boolean>;
+export type UserWithoutPassword = Omit<User, 'password'>;
+
+export type UsersQueryParams = {
+  email?: string;
+  name?: string;
+};
