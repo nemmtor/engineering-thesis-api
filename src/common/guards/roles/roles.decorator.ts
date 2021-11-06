a48d123f2406b920014ee8d348070550ca6b7b0e
nemmtor <kacper0witas@gmail.com>
2021-11-06 22:22:56 +0100

    add login for mobile app

--- src/common/guards/roles/roles.decorator.ts
+++ src/common/guards/roles/roles.decorator.ts
@@ -2,4 +2,5 @@ import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '@prisma/client';
 
 export const rolesKey = 'roles';
-export const Roles = (...roles: UserRole[]) => SetMetadata(rolesKey, roles);
+export const Roles = (...roles: UserRole[]) =>
+  SetMetadata(rolesKey, [UserRole.ADMIN, ...roles]);
